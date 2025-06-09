cc5c291f70eb70daaa4600765c5172f532f35f6e
Arunprakash-06 <amarunprakash06@gmail.com>
2025-06-09 18:08:38 +0530

    Delete Test -6/Task - 06 directory

--- Test -6/Task - 06/Question-1/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from 'react-dom/client';
-import welcome from "./welcome";
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<welcome name = "ArunPrakash" />)
--- Test -6/Task - 06/Question-2/Hellocomponent.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const HelloComponent = () => {
-  return <p>Hello from HelloComponent</p>
-};
-
-export default HelloComponent;
