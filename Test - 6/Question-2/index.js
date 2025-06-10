acaa9e1c0f264fb170043852bc6d45a0da91fb3f
Arunprakash-06 <amarunprakash06@gmail.com>
2025-06-10 20:48:47 +0530

    Create index.js

---
+++ Test - 6/Question-2/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import HelloComponent from "./Hellocomponent";
+import App from "./app";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
