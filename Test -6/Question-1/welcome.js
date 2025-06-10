1841962984c0e44e3a7ffcd0c2ba1c9b7e373c4f
Arunprakash-06 <amarunprakash06@gmail.com>
2025-06-10 20:42:49 +0530

    Delete Test -6 directory

--- Test -6/Question-1/welcome.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const welcome = ({name}) => {
-  return <h1>welcome, {name}!</h1>;
-}
-
-export default welcome;
--- Test -6/Question-2/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import HelloComponent from "./Hellocomponent";
-import App from "./app";
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
