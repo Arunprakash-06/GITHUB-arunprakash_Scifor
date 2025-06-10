411d0ad36ed6f8de39f2d10a8d2f92a1a9e9b7eb
Arunprakash-06 <amarunprakash06@gmail.com>
2025-06-10 20:47:21 +0530

    Create app.js

---
+++ Test - 6/Question-2/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import HelloComponent from "./Hellocomponent";
+
+const App = () => {
+  return(
+    <div>
+      <HelloComponent />
+    </div>
+  );
+};
+
+export default App;
